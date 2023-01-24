87b6ae784de56f9b2c7f4d7f4f6508511b86bebe
JaeYoung <chlwodud0327@naver.com>
2023-01-24 16:28:13 +0900

    Boolean , null , undefined

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ console.log(myName*2);
 console.log(myName/2);
  */
 
+/**
+ * Boolean : true or false
+ * null : 값이 비어잆다.
+ * undefined : 선언만되고 정의되지 않은 변수
+ 
+const amIFat = false;
+const isNull = null; //null 과 false는 다르다.
+
+let something; //undefined 선언되지않은상태
+
+console.log(amIFat);
+*/
+
