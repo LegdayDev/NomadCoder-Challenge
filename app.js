c222cb7be19b19474e8012486f273f0fa90ef921
JaeYoung <chlwodud0327@naver.com>
2023-01-24 16:27:19 +0900

    데이터타입(정수,문자) let, const

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("hi");
+/**
+ * integer(정수)
+ * string(문자)
+
+const num = 5;
+let myName = "nico";
+
+console.log(myName+2);
+console.log(myName*2);
+console.log(myName/2);
+
+myName = 'changed_nico';
+
+console.log(myName+2);
+console.log(myName*2);
+console.log(myName/2);
+ */
+
