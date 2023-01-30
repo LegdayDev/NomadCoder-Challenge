c81b4fa9c173eea01eb226509ac438e10be49c3c
JaeYoung <chlwodud0327@naver.com>
2023-01-30 15:34:47 +0900

    classList 를 사용한 기존 class명을 유지'

--- app.js
+++ app.js
@@ -275,14 +275,12 @@ window.addEventListener("online", handleWindowOnline);
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-   const currentColor = h1.style.color;
-   let newColor;
-   if(currentColor === "blue"){
-      newColor = "tomato";
-   } else{
-      newColor = "blue";
+   const clickedClass = "clicked";
+   if(h1.classList.contains(clickedClass)){
+      h1.classList.remove(clickedClass);
+   }else{
+      h1.classList.add(clickedClass);
    }
-   h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
