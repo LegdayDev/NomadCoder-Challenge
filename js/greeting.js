47000589503c5a5bab1963919079830f0ea4aca8
JaeYoung <chlwodud0327@naver.com>
2023-02-02 19:29:12 +0900

    파일 분류

---
+++ js/greeting.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+   event.preventDefault(); //submit의 새로운창을 띄우는 기능을 막는다.
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
+   localStorage.setItem(USERNAME_KEY, username); // localstorage : 브라우저에 저장되는 작은 DB ( key : value )
+   paintGreetings(username);
+}
+function paintGreetings(username) {
+   greeting.innerText = `Hello ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+   paintGreetings(savedUsername);
+}
