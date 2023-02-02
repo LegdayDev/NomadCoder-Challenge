548b9c5922a820e9fd62a2f9cf23fe91b1842738
JaeYoung <chlwodud0327@naver.com>
2023-02-02 22:45:37 +0900

    Background 설정

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
-const images = ["0.jped", "1.jped", "2.jpeg"];
+const images = ["01.jpeg", "02.jpeg", "03.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.querySelector("img");
-
+const bgImage = document.createElement("img");
+console.log(chosenImage);
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage); //body 제일 끝에 태그를 추가.
